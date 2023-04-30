0a981f14e31c445982ff6574f236f3b739e0237d
mahmoud lotfy <mahmoudlotfy3799@gmail.com>
2023-04-30 15:49:59 +0300

    modified to access needed vars for testing

--- src/Employee.java
+++ src/Employee.java
@@ -1,26 +1,19 @@
-
 import java.util.ArrayList;
+
 public class Employee extends Person {
     
-
     private double baseSalary;
- 
-    private ArrayList<Employee> EmployeeList = new ArrayList<Employee>();
-
+    private ArrayList<Employee> employeeList = new ArrayList<Employee>();
+    
     public Employee() {
         super(null, 0);
     }
     
-    
-    
-    public Employee(String Name, int ID, double baseSalary) {
-        super(Name, ID);
-        
-        this.baseSalary= baseSalary;
-       
+    public Employee(String name, int id, double baseSalary) {
+        super(name, id);
+        this.baseSalary = baseSalary;
     } 
-   
-
+    
     public double getBaseSalary() {
         return baseSalary;
     }
@@ -29,19 +22,15 @@ public class Employee extends Person {
         this.baseSalary = baseSalary;
     }
     
-  
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
     
     @Override
     public void printPerson() {
-        System.out.println("Employee name: "+ getName());
-        System.out.println("Employee ID: "+ getID());
-        System.out.println("Employee Base salary: "+ getBaseSalary());
-    
+        System.out.println("Employee name: " + getName());
+        System.out.println("Employee ID: " + getID());
+        System.out.println("Employee Base salary: " + getBaseSalary());
     }
     
- 
 }
-
-  
-
-
